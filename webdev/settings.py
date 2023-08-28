7e2fbf2ce382140db21cee2139fd2cb792911b30
rafael <silva.rafa@hotmail.com>
2023-08-27 22:25:13 -0300

    passando as ultimas atualizações para main

--- webdev/settings.py
+++ webdev/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
